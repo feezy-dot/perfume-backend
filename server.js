2eb46ee4f85d8baa3c01ff0b9bab3e48718ce441
feezy-dot <hafizwakili002@gmail.com>
2026-01-02 09:59:42 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend alive");
+});
+
+app.post("/paystack-webhook", (req, res) => {
+  console.log("Webhook received");
+  res.status(200).json({ received: true });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
