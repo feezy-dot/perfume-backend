e7f643f8f7399a970d5fcec9c881d86345a2326b
feezy-dot <hafizwakili002@gmail.com>
2026-01-11 18:22:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 
 // Health check (very important)
@@ -10,13 +10,12 @@ app.get("/", (req, res) => {
   res.send("Backend alive");
 });
 
-// Paystack webhook
 app.post("/paystack-webhook", (req, res) => {
   console.log("Webhook received");
   res.status(200).json({ received: true });
 });
 
-// Start server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
